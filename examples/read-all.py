79234bca23b492bf51870d62176b9e73d0ba8d27
Sandy Macdonald <sandyjmacdonald@gmail.com>
2019-04-26 21:32:55 +0100

    Adding read-all example

---
+++ examples/read-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import time
+from icm20948 import ICM20948
+
+imu = ICM20948()
+
+while True:
+    x, y, z = imu.read_magnetometer_data()
+    ax, ay, az, gx, gy, gz = imu.read_accelerometer_gyro_data()
+
+    print("""
+Accel: {:05.2f} {:05.2f} {:05.2f}
+Gyro:  {:05.2f} {:05.2f} {:05.2f}
+Mag:   {:05.2f} {:05.2f} {:05.2f}""".format(
+        ax, ay, az, gx, gy, gz, x, y, z
+        ))
+
+    time.sleep(0.25)
