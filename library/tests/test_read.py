271460a0a9e96e3a5336f3b0cf77cf50e01730c7
Danny Staple <danny@orionrobots.co.uk>
2020-06-12 23:20:17 +0100

    Adding a test for the temperature

--- library/tests/test_read.py
+++ library/tests/test_read.py
@@ -13,3 +13,13 @@ def test_accel_gyro():
     ax, ay, az, gx, gy, gz = icm20948.read_accelerometer_gyro_data()
     assert (round(ax, 2), round(ay, 2), round(az, 2), int(gx), int(gy), int(gz)) == (0.05, 0.11, 0.16, 3, 4, 5)
     del icm20948
+
+def test_temperature():
+    smbus = mock.Mock()
+    smbus.SMBus = MockSMBus
+    sys.modules['smbus'] = smbus
+
+    from icm20948 import ICM20948
+    icm20948 = ICM20948()
+    temp_degrees = icm20948.read_temperature()
+    assert (round(temp_degrees, 2) = 24)
