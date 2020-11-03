a6e69d40c7c728b160b272ee0a749a8054eada2f
Phil Howard <phil@gadgetoid.com>
2020-11-03 14:12:40 +0000

    Lint tests

--- library/tests/test_read.py
+++ library/tests/test_read.py
@@ -1,7 +1,3 @@
-import sys
-import mock
-
-
 def test_accel_gyro(smbus):
     from icm20948 import ICM20948
     icm20948 = ICM20948()
@@ -9,12 +5,14 @@ def test_accel_gyro(smbus):
     assert (round(ax, 2), round(ay, 2), round(az, 2), int(gx), int(gy), int(gz)) == (0.05, 0.11, 0.16, 3, 4, 5)
     del icm20948
 
+
 def test_magnetometer(smbus):
     from icm20948 import ICM20948
     icm20948 = ICM20948()
     x, y, z = icm20948.read_magnetometer_data()
     assert (round(x, 2), round(y, 2), round(z, 2)) == (16.65, 33.3, 49.95)
     del icm20948
+
 
 def test_temperature(smbus):
     from icm20948 import ICM20948
